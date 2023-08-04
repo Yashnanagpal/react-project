20811b140158727ecb969ebb7efd361de78eedd4
Yashnanagpal <104733123+Yashnanagpal@users.noreply.github.com>
2023-08-04 21:45:23 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MoviesList from './CompletedProjects/MoviesList.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <MoviesList />
+);
